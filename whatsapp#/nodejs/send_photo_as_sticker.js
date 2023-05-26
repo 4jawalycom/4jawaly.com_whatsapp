cf03d0a54a5521231799ae13e7fc770550f8e958
4jawaly.com <s.adel@4jawaly.net>
2023-05-27 01:52:14 +0300

    send_photo_as_sticker

---
+++ whatsapp#/nodejs/send_photo_as_sticker.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+
+const domain = "https://wa-XXX.4jawaly.com/";
+const params = {
+    token: "XXXXXXX",
+    phone: "19292439373",
+    body: "https://i0.wp.com/www.4jawaly.com/wp-content/uploads/2019/05/itservice2_pic2.png",
+    quotedMsgId: "",
+    sendMediaAsSticker: 1,
+    filename: "photo_name.png",
+    stickerName: "4jawaly sticker"
+};
+
+if (!params.token) {
+    console.log("token: It must not be a null value");
+    process.exit();
+}
+
+if (!params.phone) {
+    console.log("phone: It must not be a null value");
+    process.exit();
+}
+
+if (!params.body) {
+    console.log("body: It must not be a null value");
+    process.exit();
+}
+
+const url = `${domain}api/v1/message/file`;
+const headers = { Accept: "application/json" };
+
+axios.post(url, params, { headers })
+    .then(response => {
+        const result = response.data;
+        const code = result.code;
+        const message = response.status === 200 ? "تم الارسال بنجاح" : result.message;
+        const id = result.id;
+
+        console.log(`code: ${code}`);
+        console.log(`message: ${message}`);
+        console.log(`id: ${id}`);
+    })
+    .catch(error => {
+        if (error.response) {
+            const result = error.response.data;
+            const code = result.code;
+            const message = result.message;
+
+            console.log(`code: ${code}`);
+            console.log(`message: ${message}`);
+        } else {
+            console.log(error.message);
+        }
+    });
---
+++ whatsapp#/ruby/send_photo_as_file.rb
@@ -0,0 +1,47 @@
+require 'net/http'
+require 'json'
+
+domain = "https://wa-XXX.4jawaly.com/"
+params = {
+  "token" => "XXXXXXX",
+  "phone" => "19292439373",
+  "body" => "https://i0.wp.com/www.4jawaly.com/wp-content/uploads/2019/05/itservice2_pic2.png",
+  "quotedMsgId" => "",
+  "filename" => "photo_name.png",
+  "sendMediaAsDocument" => 1,
+  "stickerName" => "stickerName"
+}
+
+if params["token"].empty?
+  puts "token: It must not be a null value"
+  exit
+end
+
+if params["phone"].empty?
+  puts "phone: It must not be a null value"
+  exit
+end
+
+if params["body"].empty?
+  puts "body: It must not be a null value"
+  exit
+end
+
+url = "#{domain}api/v1/message/file"
+headers = { "Accept" => "application/json" }
+
+uri = URI(url)
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.path, headers)
+request.set_form_data(params)
+
+response = http.request(request)
+result = JSON.parse(response.body)
+
+code = result["code"]
+message = response.code == "200" ? "تم الارسال بنجاح" : result["message"]
+id = result["id"]
+
+puts "code: #{code}"
+puts "message: #{message}"
+puts "id: #{id}"
